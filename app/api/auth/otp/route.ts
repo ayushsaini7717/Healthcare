afbf8e14a890a1d5b0028a50a9e4cfa468b57f66
Ayush saini <ayushsaini7717@gmail.com>
2025-09-23 23:04:30 +0530

    fix/auth-otp

--- app/api/auth/otp/route.ts
+++ app/api/auth/otp/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import nodemailer from 'nodemailer';
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
+import prisma from '@/lib/prisma';
+
 export async function POST(request: Request) {
   try {
     const { email } = await request.json();
