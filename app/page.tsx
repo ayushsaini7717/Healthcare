e40aca76787b0ebe328076f1926a5950e256d7cf
bhumipandey06 <bhumipandey1188@gmail.com>
2025-10-27 18:11:02 +0530

    added the contacts section

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,6 @@ export default function HomePage() {
         <HeroSection />
         <AboutSection/>
         <ServicesSection/>
-        
         <DoctorsSection />
         <ContactsSection />
       </main>
