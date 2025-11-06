7f1ae0ec28e231560b59b24529af992e3dc73dbc
bhumipandey06 <bhumipandey1188@gmail.com>
2025-11-06 18:06:41 +0530

    updated the doctors section

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function HomePage() {
         <HeroSection/>
         <AboutSection/>
         <ServicesSection/>
-        <DoctorsSection/>
+        {/* <DoctorsSection/> */}
         <ContactsSection/>
       </main>
 
